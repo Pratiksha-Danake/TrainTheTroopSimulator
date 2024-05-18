08b58f059a5b091064bf4f8bb65dfab954901689
Danake Pratiksha <danakepratiksha1020@gmail.com>
2024-05-18 13:19:03 +0530

    added method in Barracks controller to add troopers to barracks

---
+++ TrainTheTroopSimulation/src/main/java/com/amaap/trainthetroop/controller/BarracksController.java
@@ -0,0 +1,26 @@
+package com.amaap.trainthetroop.controller;
+
+import com.amaap.trainthetroop.controller.dto.Response;
+import com.amaap.trainthetroop.controller.valueobject.HttpStatus;
+import com.amaap.trainthetroop.domain.service.BarracksService;
+import com.amaap.trainthetroop.domain.service.exception.TrooperLimitExceededException;
+import com.google.inject.Inject;
+
+public class BarracksController {
+    private BarracksService barracksService;
+
+    @Inject
+    public BarracksController(BarracksService barracksService) {
+        this.barracksService = barracksService;
+    }
+
+    public Response addTroopersToBarrack(int archerCount, int barbarianCount) {
+        try {
+            barracksService.addTroopersToBarrack(archerCount, barbarianCount);
+            return new Response(HttpStatus.OK, "Added");
+
+        } catch (TrooperLimitExceededException e) {
+            return new Response(HttpStatus.BAD_REQUEST, "Added");
+        }
+    }
+}
--- TrainTheTroopSimulation/src/test/java/com/amaap/trainthetroop/controller/BarracksControllerTest.java
+++ TrainTheTroopSimulation/src/test/java/com/amaap/trainthetroop/controller/BarracksControllerTest.java
@@ -1,5 +1,46 @@
 package com.amaap.trainthetroop.controller;
 
-public class BarracksControllerTest {
+import com.amaap.trainthetroop.AppModule;
+import com.amaap.trainthetroop.controller.dto.Response;
+import com.amaap.trainthetroop.controller.valueobject.HttpStatus;
+import com.amaap.trainthetroop.domain.model.entity.Troop;
+import com.amaap.trainthetroop.domain.model.entity.exception.InvalidTrainingCostException;
+import com.amaap.trainthetroop.domain.model.entity.exception.InvalidTrainingTimeException;
+import com.amaap.trainthetroop.domain.model.entity.exception.InvalidTrooperWeaponException;
+import com.amaap.trainthetroop.service.TrooperService;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BarracksControllerTest {
+    private BarracksController barracksController;
+    private TrooperService trooperService;
+
+    @BeforeEach
+    void initialize() {
+        Injector injector = Guice.createInjector(new AppModule());
+        barracksController = injector.getInstance(BarracksController.class);
+        trooperService = injector.getInstance(TrooperService.class);
+    }
+
+    @Test
+    void shouldGetResponseAsOkIfAddsTrooperSuccessfullyToTheBarracks() throws InvalidTrainingTimeException, InvalidTrainingCostException, InvalidTrooperWeaponException {
+        // arrange
+        int archerCount = 5;
+        int barbarianCount = 5;
+        for (int i = 0; i < 5; i++) {
+            trooperService.createTrooper(Troop.ARCHER, 6, 20, "Bow and Arrow");
+            trooperService.createTrooper(Troop.BARBARIAN, 3, 10, "Sword");
+        }
+        Response expected = new Response(HttpStatus.OK, "Added");
+
+        // act
+        Response actual = barracksController.addTroopersToBarrack(archerCount, barbarianCount);
+
+        // assert
+        assertEquals(expected, actual);
+    }
 }
