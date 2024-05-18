2359d26264547800a57efcf59c27958abc0b02b8
Danake Pratiksha <danakepratiksha1020@gmail.com>
2024-05-18 15:34:30 +0530

    added test case to get trooper of specified count at repository level

--- TrainTheTroopSimulation/src/test/java/com/amaap/trainthetroop/repository/impl/InMemoryTrooperRepositoryTest.java
+++ TrainTheTroopSimulation/src/test/java/com/amaap/trainthetroop/repository/impl/InMemoryTrooperRepositoryTest.java
@@ -2,6 +2,7 @@ package com.amaap.trainthetroop.repository.impl;
 
 import com.amaap.trainthetroop.AppModule;
 import com.amaap.trainthetroop.domain.model.entity.Archer;
+import com.amaap.trainthetroop.domain.model.entity.Barbarian;
 import com.amaap.trainthetroop.domain.model.entity.Trooper;
 import com.amaap.trainthetroop.domain.model.entity.exception.InvalidTrainingCostException;
 import com.amaap.trainthetroop.domain.model.entity.exception.InvalidTrainingTimeException;
@@ -10,6 +11,8 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -35,5 +38,22 @@ class InMemoryTrooperRepositoryTest {
 
         // assert
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldBeAbleToGetTroopersOfSpecifiedCount() throws InvalidTrainingTimeException, InvalidTrainingCostException, InvalidTrooperWeaponException {
+        // arrange
+        int expectedArcherCount = 5;
+        int expectedBarbarianCount = 5;
+        for (int i = 0; i < 5; i++) {
+            inMemoryTrooperRepository.addTrooper(new Archer(6, 20, "Bow and Arrow"));
+            inMemoryTrooperRepository.addTrooper(new Barbarian(3, 10, "Sword"));
+        }
+
+        // act
+        List<Trooper> troopers = inMemoryTrooperRepository.getTroopersOfCount(expectedArcherCount, expectedBarbarianCount);
+
+        // assert
+        assertEquals(10, troopers.size());
     }
 }
